fba678110f20d2200aeb171d8ee359207e1cf298
vangona <rlarhksrud12@naver.com>
2021-08-15 17:00:15 +0900

    2021.08.15

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
-import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js";
-import { PerspectiveCamera, PointLight } from "https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js";
-
+const canvas = document.querySelector("#c")
 const scene = new THREE.Scene();
-
 const renderer = new THREE.WebGLRenderer({
-    alpha: true,
+    canvas,
     antialias: true
 })
 
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight
+
+renderer.setSize(WIDTH, HEIGHT)
+
+const FOV = 20
+const ASPECT = WIDTH / HEIGHT
+const NEAR = 0.1
+const FAR = 10000
+
+const camera = new THREE.PerspectiveCamera(
+    20,
+    ASPECT,
+    NEAR,
+    FAR
+)
+camera.position.set(0, 10, 500);
 
 const RADIUS = 40
 const geometry = new THREE.IcosahedronGeometry(RADIUS, 0);
@@ -15,45 +29,46 @@ const geometry = new THREE.IcosahedronGeometry(RADIUS, 0);
 const material = new THREE.MeshLambertMaterial({ color : "purple" })
 
 const mesh = new THREE.Mesh(geometry, material);
-
 scene.add(mesh)
-mesh.position.z = -RADIUS * 10
 
-const WIDTH = 500
-const HEIGHT = 500
+const ambientLight = new THREE.AmbientLight(0x404040, 3)
+ambientLight.position.set(100, 100, 100)
+scene.add(ambientLight)
 
-const FOV = 20
-const ASPECT = WIDTH / HEIGHT
-const NEAR = 0.1
-const FAR = 10000
-
-const camera = new PerspectiveCamera(
-    FOV,
-    ASPECT,
-    NEAR,
-    FAR
-)
-
-renderer.setSize(WIDTH, HEIGHT)
-
-const container = document.querySelector('#three')
-container.appendChild(renderer.domElement)
-
-const pointLight = new PointLight(0xFFFFF, 1)
-
-pointLight.position.x = 300
-pointLight.position.y = 300
-pointLight.position.z = 30
-
+const pointLight = new THREE.PointLight(0xffffff, 2)
+pointLight.position.set(100, 100, 100)
 scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xffffff, 2)
+pointLight.position.set(-100, -100, -100)
+scene.add(pointLight2)
+
+const pointLight3 = new THREE.PointLight(0xffffff, 2)
+pointLight.position.set(-100, -100, -100)
+scene.add(pointLight3)
+
+const controls = new THREE.OrbitControls(camera, canvas);
+controls.mouseButtons = {
+    LEFT: THREE.MOUSE.ROTATE,
+    MIDDLE: THREE.MOUSE.DOLLY,
+    RIGHT: THREE.MOUSE.PAN
+}
+controls.update()
+
+const axesHelper = new THREE.AxesHelper(1000);
+scene.add(axesHelper)
+
+const gridHelper = new THREE.GridHelper(1000, 10);
+scene.add(gridHelper)
 
 const update = () => {
     const speed = Math.random() / 200
     mesh.rotation.x += speed
     mesh.rotation.y += speed
     mesh.rotation.z += speed
-    renderer.render(scene, camera)
     requestAnimationFrame(update)
+    controls.update()
+    renderer.render(scene, camera)
 }
 
 requestAnimationFrame(update)
